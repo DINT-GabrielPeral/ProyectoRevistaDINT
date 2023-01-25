c2cb50800014428bb039b57468e8386ed2cd6590
PABLO SEMPERE MARTÍNEZ <pabsemmar@alu.edu.gva.es>
2023-01-25 19:15:18 +0100

    Creadas las vistas modelo iniciales y la clase Autor

---
+++ ProyectoRevistaDINT/Clases/Autor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoRevistaDINT.Clases
+{
+    class Autor : ObservableObject
+    {
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { SetProperty(ref nombre, value); }
+        }
+
+        private string imagen;
+        public string Imagen
+        {
+            get { return imagen; }
+            set { SetProperty(ref imagen, value); }
+        }
+
+        private string redSocial;
+        public string RedSocial
+        {
+            get { return redSocial; }
+            set { SetProperty(ref redSocial, value); }
+        }
+    }
+}
---
+++ ProyectoRevistaDINT/VistasModelo/EliminarAutorVM.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using ProyectoRevistaDINT.Clases;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoRevistaDINT.VistasModelo
+{
+    class EliminarAutorVM : ObservableObject
+    {
+        private ObservableCollection<Autor> listaAutores;
+        public ObservableCollection<Autor> ListaAutores
+        {
+            get { return listaAutores; }
+            set { SetProperty(ref listaAutores, value); }
+        }
+
+        public EliminarAutorVM()
+        {
+            //Aquí se carga la lista de autores desde la BD
+            //listaAutores =
+        }
+    }
+}
