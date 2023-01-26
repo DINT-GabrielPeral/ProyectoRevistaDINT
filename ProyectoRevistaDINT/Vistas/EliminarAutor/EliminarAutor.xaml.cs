b8b8faab5527c88faea6d74498aee2a88ed07bf9
JAVIER DE MENA ASENJO <javdemase@alu.edu.gva.es>
2023-01-26 12:39:24 +0100

    Creado eliminarAutor

---
+++ ProyectoRevistaDINT/Vistas/EliminarAutor/EliminarAutor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoRevistaDINT.Vistas.EliminarAutor
+{
+    /// <summary>
+    /// Lógica de interacción para EliminarAutor.xaml
+    /// </summary>
+    public partial class EliminarAutor : Window
+    {
+        public EliminarAutor()
+        {
+            InitializeComponent();
+        }
+    }
+}
