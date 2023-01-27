df1b07ba2fe0a5a87d44074685762b42fff7de6b
alumno <alumno@A301PC14>
2023-01-27 12:47:49 +0100

    Creada la clase Articulo. Creado un servicio para las secciones. Creado el XAML y VM de crear artículos.

---
+++ ProyectoRevistaDINT/Servicios/SeccionesService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ProyectoRevistaDINT.Servicios
+{
+    public class SeccionesService
+    {
+        public ObservableCollection<string> GetSecciones()
+        {
+            return new ObservableCollection<string>
+            {
+                "Sección 1",
+                "Sección 2",
+                "Sección 3"
+            };
+        }
+    }
+}
---
+++ ProyectoRevistaDINT/Vistas/CrearArticulo/CrearArticuloWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace ProyectoRevistaDINT.Vistas.CrearArticulo
+{
+    /// <summary>
+    /// Lógica de interacción para CrearArticuloWindow.xaml
+    /// </summary>
+    public partial class CrearArticuloWindow : Window
+    {
+        private readonly CrearArticuloWindowVM vm;
+
+        public CrearArticuloWindow()
+        {
+            InitializeComponent();
+            vm = new CrearArticuloWindowVM();
+            DataContext = vm;
+        }
+    }
+}
