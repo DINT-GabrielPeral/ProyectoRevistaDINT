66f5b06d6709e35f8d3469d4f3b3a11e199be0b3
PABLO SEMPERE MARTÍNEZ <pabsemmar@alu.edu.gva.es>
2023-01-30 10:19:42 +0100

    Creacion de la sección de Artículos en la BD

--- ProyectoRevistaDINT/Servicios/ServicioAccesoBD.cs
+++ ProyectoRevistaDINT/Servicios/ServicioAccesoBD.cs
@@ -29,6 +29,15 @@ namespace ProyectoRevistaDINT.Servicios
                                     nickRedSocial varchar(100))";
             comando.ExecuteNonQuery(); //Este método ejecuta consultas que no son SELECT
 
+            SqliteCommand comando2 = conexion.CreateCommand();
+            comando2.CommandText = @"CREATE TABLE IF NOT EXISTS articulo (
+                                    titulo varchar(100) primary key,
+                                    imagen varchar(500), 
+                                    texto varchar(1000),
+                                    seccion varchar(100),
+                                    autorArticulo integer,
+                                    CONSTRAINT fk_autor FOREIGN KEY (autorArticulo) REFERENCES autor(id))";
+            comando2.ExecuteNonQuery();
 
             conexion.Close();
         }
@@ -83,6 +92,60 @@ namespace ProyectoRevistaDINT.Servicios
             comando.Parameters["@redSocial"].Value = autor.RedSocial;
             comando.Parameters["@nickRedSocial"].Value = autor.NickRedSocial;
             comando.Parameters["@id"].Value = autor.Id;
+            comando.ExecuteNonQuery();
+
+            conexion.Close();
+        }
+        public ObservableCollection<Articulo> recibirArticulos()
+        {
+            ObservableCollection<Articulo> listaArticulos = new ObservableCollection<Articulo>();
+
+            SqliteConnection conexion = new SqliteConnection("Data Source=DatosRevista.db");
+            conexion.Open();
+
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = "SELECT * FROM articulo";
+            SqliteDataReader lector = comando.ExecuteReader();
+            int autorArticulo;
+            string titulo, imagen, texto, seccion;
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    //Distintas formas de acceder a los campos de la fila actual
+                    titulo = (string)lector["titulo"];
+                    imagen = (string)lector["imagen"];
+                    texto = (string)lector["texto"];
+                    seccion = (string)lector["seccion"];
+                    autorArticulo = int.Parse(lector["autorArticulo"].ToString());
+                    listaArticulos.Add(new Articulo(titulo, imagen, texto, seccion, autorArticulo));
+                }
+            }
+
+            conexion.Close();
+
+            return listaArticulos;
+        }
+
+        public void crearArticulo(Articulo articulo)
+        {
+            SqliteConnection conexion = new SqliteConnection("Data Source=DatosRevista.db");
+            conexion.Open();
+
+            SqliteCommand comando = conexion.CreateCommand();
+
+            comando.CommandText = "INSERT INTO autor VALUES (@titulo,@imagen,@texto,@seccion,@autorArticulo)";
+            comando.Parameters.Add("@titulo", SqliteType.Text);
+            comando.Parameters.Add("@imagen", SqliteType.Text);
+            comando.Parameters.Add("@texto", SqliteType.Text);
+            comando.Parameters.Add("@seccion", SqliteType.Text);
+            comando.Parameters.Add("@autorArticulo", SqliteType.Integer);
+            comando.Parameters["@titulo"].Value = articulo.Titulo;
+            comando.Parameters["@imagen"].Value = articulo.Imagen;
+            comando.Parameters["@texto"].Value = articulo.Texto;
+            comando.Parameters["@seccion"].Value = articulo.Seccion;
+            comando.Parameters["@autorArticulo"].Value = articulo.AutorArticulo;
             comando.ExecuteNonQuery();
 
             conexion.Close();
