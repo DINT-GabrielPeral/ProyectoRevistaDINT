5767325ae66d8a6a3bd844f0a0e8250dcf0b54da
PabloS <pablo.sempere@iesdoctorbalmis.com>
2023-02-01 12:55:25 +0100

    Servicio Azure

---
+++ ProyectoRevistaDINT/Servicios/AzureService.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoRevistaDINT.Servicios
+{
+    class AzureService
+    {
+        public String SubirImagen(String rutaImagenLocal)
+        {
+            string cadenaConexion = "DefaultEndpointsProtocol=https;AccountName=trivialpablos;AccountKey=E3/ytDcBrNy5/XqhlF8stLFgAMnJT+QBz19F6WymeCcNxJJ31YlmgrMpCK+S/LYnt8R2no8GdrSF+AStSAOp5Q==;EndpointSuffix=core.windows.net"; //La obtenemos en el portal de Azure, asociada a la cuenta de almacenamiento
+            string nombreContenedorBlobs = "revista"; //El nombre que le hayamos dado a nuestro contenedor de blobs en el portal de Azure
+            string rutaImagen = rutaImagenLocal;
+
+            //Obtenemos el cliente del contenedor
+            var clienteBlobService = new BlobServiceClient(cadenaConexion);
+            var clienteContenedor = clienteBlobService.GetBlobContainerClient(nombreContenedorBlobs);
+
+            //Leemos la imagen y la subimos al contenedor
+            Stream streamImagen = File.OpenRead(rutaImagen);
+            string nombreImagen = Path.GetFileName(rutaImagen);
+            clienteContenedor.UploadBlob(nombreImagen, streamImagen);
+
+            //Una vez subida, obtenemos la URL para referenciarla
+            var clienteBlobImagen = clienteContenedor.GetBlobClient(nombreImagen);
+            string urlImagen = clienteBlobImagen.Uri.AbsoluteUri;
+
+            return urlImagen;
+        }
+    }
+}
