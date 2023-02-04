d7eb66e3277b374b632d6ea1c9cd41befe28b589
Javier de Mena Asenjo <javier.demena@iesdoctorbalmis.com>
2023-02-04 18:55:46 +0100

    Añadida libreria generacion de PDF

--- ProyectoRevistaDINT/Servicios/ServicioPDF.cs
+++ ProyectoRevistaDINT/Servicios/ServicioPDF.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace ProyectoRevistaDINT.Servicios
 {
     class ServicioPDF
     {
+        public void generarPDF()
+        {
+            PdfDocument document = new PdfDocument();
+
+            PdfPage page = document.AddPage();
+
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+            gfx.DrawString("TITULO", font, XBrushes.Black,
+                    new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+
+            string filename = "NOMBRE_PDF.pdf";
+
+            document.Save(filename);
+
+            //Process.Start(filename);
+        }
     }
 }
