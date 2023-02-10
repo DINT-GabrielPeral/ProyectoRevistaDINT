d08e63ff0f53b20f2d74f876e70cb7978f667202
JAVIER DE MENA ASENJO <javdemase@alu.edu.gva.es>
2023-02-10 12:15:57 +0100

    Mejora pdf

--- ProyectoRevistaDINT/Servicios/ServicioPDF.cs
+++ ProyectoRevistaDINT/Servicios/ServicioPDF.cs
@@ -70,7 +70,7 @@ namespace ProyectoRevistaDINT.Servicios
                                     });
 
                             });
-                        });
+                        }).GeneratePdf("./Borrar");
                 }
            }
 
