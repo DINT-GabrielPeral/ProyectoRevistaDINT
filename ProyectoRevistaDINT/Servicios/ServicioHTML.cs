b830bef2b9e06f91cd3e10da2c34cc2770c3df43
JAVIER DE MENA ASENJO <javdemase@alu.edu.gva.es>
2023-02-13 12:52:48 +0100

    Avances creacion web

--- ProyectoRevistaDINT/Servicios/ServicioHTML.cs
+++ ProyectoRevistaDINT/Servicios/ServicioHTML.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,26 @@ namespace ProyectoRevistaDINT.Servicios
 {
     class ServicioHTML
     {
+        public void GenerarRevista()
+        {
+            String path = @"./Borrar/web.html";
+            String revista = "<!DOCTYPE html>" +
+                "<html lang=\"es\" xmlns=\"http://www.w3.org/1999/xhtml \">" +
+                "<head>" +
+                    "<meta charset=\"utf-8\"/>" +
+                    "<title></title>" +
+                "</head>" +
+                "<body>";
+                
+                revista += "</body>" +"</html>";
+
+            using (FileStream fs = File.Create(path))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(revista);
+                fs.Write(info, 0, info.Length);
+            }
+
+
+        }
     }
 }
