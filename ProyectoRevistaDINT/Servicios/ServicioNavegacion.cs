04771e2568416d9f6ccf39d48ad688b453be6da5
DANIEL STIVEN CARABALI ORDÓÑEZ <dancarord@alu.edu.gva.es>
2023-02-15 18:42:42 +0100

    Añadida la documentación del servicio de navegación.

--- ProyectoRevistaDINT/Servicios/ServicioNavegacion.cs
+++ ProyectoRevistaDINT/Servicios/ServicioNavegacion.cs
@@ -38,27 +38,44 @@ namespace ProyectoRevistaDINT.Servicios
             return new GestionArticulos();
         }
 
+        /// <summary>
+        /// Este método sirve para cargar un diálogo de pregunta diciendo si está seguro de que quiere eliminar el autor seleccionado.
+        /// </summary>
+        /// <returns>Este método devuelve un booleano indicando si el usuario le ha dado a Sí (true) o a No (false).</returns>
         public bool? AbrirEliminarAutor()
         {
             return MessageBox.Show(
                 "¿Estás seguro de que desea eliminar este autor?",
                 "ELIMINAR AUTOR",
-                MessageBoxButton.OKCancel,
-                MessageBoxImage.Exclamation) == MessageBoxResult.OK;
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Exclamation) == MessageBoxResult.Yes;
 
         }
 
+        /// <summary>
+        /// Este método sirve para cargar la vista inicial de la aplicación de la revista.
+        /// </summary>
+        /// <returns>Este método devuelve la vista inicial como un UserControl.</returns>
         internal UserControl CargaInicio()
         {
             return new InicioUC();
         }
 
+        /// <summary>
+        /// Este método sirve para cargar la vista de crear un autor como un diálogo.
+        /// </summary>
+        /// <returns>Este método devuelve un booleano indicando si el usuario ha creado el autor (true) o no (false).</returns>
         public bool? AbrirCrearAutor()
         {
             CrearAutorVentana dialog = new CrearAutorVentana();
             return dialog.ShowDialog();
         }
 
+        /// <summary>
+        /// Este método sirve para cargar la vista de editar un autor como un diálogo.
+        /// </summary>
+        /// <param name="autorSeleccionado">En este parámetro se recibe el autor que haya seleccionado el usuario para editarle sus datos.</param>
+        /// <returns>Este método devuelve un booleano indicando si el usuario ha editado el autor (true) o no (false).</returns>
         public bool? AbrirEditarAutor(Autor autorSeleccionado)
         {
             EditarAutorWindow dialog = new EditarAutorWindow();
@@ -66,31 +83,52 @@ namespace ProyectoRevistaDINT.Servicios
             return dialog.ShowDialog();
         }
 
+        /// <summary>
+        /// Este método sirve para cargar la vista de publicar un artículo.
+        /// </summary>
+        /// <returns>Este método devuelve la vista de publicar un artículo como un UserControl.</returns>
         internal UserControl AbrirPublicarArticulo()
         {
             return new PublicarArticuloUserControl1();
         }
 
+        /// <summary>
+        /// Este método sirve para cargar la vista de crear un artículo.
+        /// </summary>
+        /// <returns>Este método devuelve la vista de crear un artículo como un UserControl.</returns>
         public UserControl AbrirCrearArticulo()
         {
             return new CrearArticuloUserControl();
         }
 
+        /// <summary>
+        /// Este método sirve para cargar la vista de eliminar un artículo como un diálogo.
+        /// </summary>
+        /// <returns>Este método devuelve un booleano indicando si el usuario ha eliminado el autor (true) o no (false).</returns>
         internal bool? AbrirEliminarArticulo()
         {
             return MessageBox.Show(
                 "¿Estás seguro de que desea eliminar este artículo?",
                 "ELIMINAR ARTÍCULO",
-                MessageBoxButton.OKCancel,
-                MessageBoxImage.Exclamation) == MessageBoxResult.OK;
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Exclamation) == MessageBoxResult.Yes;
         }
 
+        /// <summary>
+        /// Este método sirve para cargar la vista de firmar un artículo como un diálogo.
+        /// </summary>
+        /// <returns>Este método devuelve un booleano indicando si el usuario ha firmado el artículo (true) o no (false).</returns>
         public bool? AbrirFirmarArticulo()
         {
             FirmarArticulo dialog = new FirmarArticulo();
             return dialog.ShowDialog();
         }
 
+        /// <summary>
+        /// Este método sirve para cargar la vista de moderar un artículo como un diálogo.
+        /// </summary>
+        /// <param name="articuloSeleccionado">En este parámetro se recibe el artículo que haya seleccionado el usuario para moderarle el contenido.</param>
+        /// <returns>Este método devuelve un booleano indicando si el usuario ha moderado el artículo (true) o no (false).</returns>
         public bool? AbrirModerarArticulo(Articulo articuloSeleccionado)
         {
             ModerarArticulo dialog = new ModerarArticulo();
